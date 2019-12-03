6d970c8ee380c4e6bc86b709c7601b7c43e5f075
Bruno Alberto Fusco <funobrusco@hotmail.com>
2019-12-03 15:50:50 +0100

    Add project files.

---
+++ RAR2GME/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAR2GME
+{
+    public partial class Main : ServiceBase
+    {
+        readonly EventLog eventLog = new EventLog();
+
+        public Main()
+        {
+            InitializeComponent();
+            eventLog.Source = "RAR2GME";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog.Log = "OnStart";
+        }
+
+        protected override void OnStop()
+        {
+            eventLog.Log = "OnStop";
+        }
+    }
+}
---
+++ RAR2GME/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAR2GME
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBase[] ServicesToRun = new ServiceBase[] { new Main() };
+            if (Environment.UserInteractive)
+            {
+                string parameter = string.Concat(args);
+                switch (parameter)
+                {
+                    case "--install":
+                        ManagedInstallerClass.InstallHelper(new[] { Assembly.GetExecutingAssembly().Location });
+                        break;
+                    case "--uninstall":
+                        ManagedInstallerClass.InstallHelper(new[] { "/u", Assembly.GetExecutingAssembly().Location });
+                        break;
+                }
+            }
+            else
+            {
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
